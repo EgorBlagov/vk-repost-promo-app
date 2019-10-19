8829bee04a6ca5d09c02c64e92653e3df655ed99
Egor Blagov <e.m.blagov@gmail.com>
2019-10-19 13:58:18 +0100

    Brief imports and styles organized

--- src/client/panels/brief/Brief.tsx
+++ src/client/panels/brief/Brief.tsx
@@ -1,31 +1,33 @@
+import './Brief.css';
+
 import * as React from 'react';
+
 import { Div, Panel, Group, List, Cell, Button, PanelHeader } from "@vkontakte/vkui"
-import './Brief.css'
-import api from '../../logic/api';
 import { Panels } from '../../logic/navigation';
+
+import api from '../../logic/api';
+
 export interface BriefProps {
     id: Panels;
     children?: any;
 }
 
-export const Brief = ({id, children}:BriefProps) => {
-    return <Panel
-        id={id}
-    >
+export const Brief = ({id, children}: BriefProps) => {
+    return <Panel id={id}>
         <PanelHeader>
             Промокод за репост
         </PanelHeader>
         <Group>
             <List>
                 <Cell multiline
-                    description={<Div className='centered-panel brief__header__description'>
+                    description={<Div className='brief__description tool__flex'>
                         Добавьте сервис в свое сообщество, настройте, и получите результат!
                         </Div>}>
-                    <Div  className='centered-panel brief__header'>
+                    <Div  className='brief__header tool__flex'>
                         Выдавай промокоды за репосты и подписку на свое сообщество
                     </Div>
                 </Cell>
-                <Cell className='centered-panel'>
+                <Cell className='tool__flex'>
                     <Button onClick={async () => api.install()}>Установить в сообщество</Button>
                 </Cell>
             </List>
